b8b1b0ba53968c57e38131ab4abd988cb5a53e38
Adam <126939161+AdamSniadach@users.noreply.github.com>
2023-06-13 21:01:25 +0100

    lvl 4 bcrypt

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
-//require md5
-const md5 = require("md5");
+//!require bcrypt
+const bcrypt = require("bcrypt");
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
+const saltRounds = 10;
 app.use(bodyParser.urlencoded({ extended: true }));
 const uri = "mongodb://0.0.0.0:27017/usersDB";
 mongoose
@@ -45,30 +45,42 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    // ADD MD5 TO PASSWORD
-    password: md5(req.body.password),
-  });
-  newUser
-    .save()
-    .then((docs) => {
-      // Handle the result here
-      res.render("secrets");
-    })
-    .catch((error) => {
-      // Handle any errors that occurred
-      console.error(error);
+  //! step:2  add this funcion
+  //! step:3 add  register funcion to inside a bcrypt funcion
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      // ADD MD5 TO PASSWORD
+      password: hash,
     });
+    newUser
+      .save()
+      .then((docs) => {
+        // Handle the result here
+        res.render("secrets");
+      })
+      .catch((error) => {
+        // Handle any errors that occurred
+        console.error(error);
+      });
+  });
 });
 app.post("/login", (req, res) => {
   //add md5 to password
-  const password = md5(req.body.password);
+
+  const password = req.body.password;
   const email = req.body.username;
   User.findOne({ email: email })
     .then((user) => {
-      if (user && user.password === password) {
-        res.render("secrets");
+      //!step4  : add compare funcion and replace hash with suer password
+      if (user) {
+        bcrypt.compare(password, user.password, function (err, result) {
+          //!step 5: check if password match and send website
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       } else {
         res.send("Invalid login or password"); // or redirect to a login error page
       }
